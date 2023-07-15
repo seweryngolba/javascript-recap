88af66d8cbbc6bd509b6ffdf31f29023711d36db
Seweryn Golba <sgoolb@gmail.com>
2023-07-15 12:27:50 +0200

    random numbers

--- index.js
+++ index.js
@@ -67,19 +67,33 @@
 //   document.getElementById("cLabel").innerHTML = "Side C: " + c;
 // };
 
-let count = 0;
+// let count = 0;
 
-document.getElementById("decreaseBtn").onclick = function () {
-  count -= 1;
-  document.getElementById("countLabel").innerHTML = count;
-};
+// document.getElementById("decreaseBtn").onclick = function () {
+//   count -= 1;
+//   document.getElementById("countLabel").innerHTML = count;
+// };
 
-document.getElementById("resetBtn").onclick = function () {
-  count = 0;
-  document.getElementById("countLabel").innerHTML = count;
-};
+// document.getElementById("resetBtn").onclick = function () {
+//   count = 0;
+//   document.getElementById("countLabel").innerHTML = count;
+// };
 
-document.getElementById("increaseBtn").onclick = function () {
-  count += 1;
-  document.getElementById("countLabel").innerHTML = count;
+// document.getElementById("increaseBtn").onclick = function () {
+//   count += 1;
+//   document.getElementById("countLabel").innerHTML = count;
+// };
+
+let x;
+let y;
+let z;
+
+document.getElementById("rollButton").onclick = function () {
+  x = Math.floor(Math.random() * 6) + 1;
+  y = Math.floor(Math.random() * 6) + 1;
+  z = Math.floor(Math.random() * 6) + 1;
+
+  document.getElementById("xlabel").innerHTML = x;
+  document.getElementById("ylabel").innerHTML = y;
+  document.getElementById("zlabel").innerHTML = z;
 };
