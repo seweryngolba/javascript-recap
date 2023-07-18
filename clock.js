aecc9c9d45758762e225c9f8d97183e19eeeaf05
Seweryn Golba <sgoolb@gmail.com>
2023-07-18 22:08:00 +0200

    simple clock

---
+++ clock.js
@@ -0,0 +1,28 @@
+const myLabel = document.getElementById("myLabel");
+
+update();
+setInterval(update, 1000);
+
+function update() {
+  let date = new Date();
+  myLabel.innerHTML = formatTime(date);
+
+  function formatTime(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let amOrPm = hours >= 12 ? "PM" : "AM";
+
+    hours = hours % 12 || 12;
+
+    hours = formatZeros(hours);
+    minutes = formatZeros(minutes);
+    seconds = formatZeros(seconds);
+
+    return `${hours}:${minutes}:${seconds} ${amOrPm}`;
+  }
+  function formatZeros(time) {
+    time = time.toString();
+    return time.length < 2 ? "0" + time : time;
+  }
+}
--- index.js
+++ index.js
@@ -24,16 +24,65 @@
 //   alert(`Thanks for buying course!`);
 // };
 
-let count = 0;
-let max = window.prompt("Count up to what number ?");
-max = Number(max);
+// let count = 0;
+// let max = window.prompt("Count up to what number ?");
+// max = Number(max);
 
-const myTimer = setInterval(countUp, 1000);
+// const myTimer = setInterval(countUp, 1000);
 
-function countUp() {
-  count++;
-  console.log(count);
-  if (count >= max) {
-    clearInterval(myTimer);
-  }
-}
+// function countUp() {
+//   count++;
+//   console.log(count);
+//   if (count >= max) {
+//     clearInterval(myTimer);
+//   }
+// }
+
+// let date = new Date(2023, 0, 2, 3, 4, 5);
+// let date = new Date("January 1, 2023 00:00:00");
+
+// let year = date.getFullYear();
+// let dayOfMonth = date.getDate();
+// let dayOfWeek = date.getDay();
+// let month = date.getMonth();
+// let hour = date.getHours();
+
+// date.setFullYear(2024);
+// date.setMonth(11);
+
+// date = date.toLocaleString();
+// let date = new Date();
+// document.getElementById("myLabel").innerHTML = formatTime(date);
+
+// function formatDate(date) {
+//   let year = date.getFullYear();
+//   let month = date.getMonth() + 1;
+//   let day = date.getDate();
+
+//   return `${month}/${day}/${year}`;
+// }
+
+// function formatTime(date) {
+//   let hours = date.getHours();
+//   let minutes = date.getMinutes();
+//   let seconds = date.getSeconds();
+//   let amOrPm = hours >= 12 ? "pm" : "am";
+
+//   hours = hours % 12 || 12;
+
+//   return `${hours}:${minutes}:${seconds} ${amOrPm}`;
+// }
+
+// console.time("response time");
+
+// //alert("Click ok button");
+// setTimeout(() => console.log("Hello"), 3000);
+
+// console.timeEnd("response time");
+
+const wait = (time) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+
+wait(3000).then(() => console.log("thanks"));
