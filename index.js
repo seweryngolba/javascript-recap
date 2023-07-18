0d384ca05470e06ca891440ba34dec7d0b3b3c73
Seweryn Golba <sgoolb@gmail.com>
2023-07-18 18:24:16 +0200

    objects

--- index.js
+++ index.js
@@ -1,100 +1,29 @@
-// let students = ["jon", "pedro", "james"];
-// students.forEach(capitalize);
-// students.forEach(print);
+const car1 = {
+  model: "Mustang",
+  color: "red",
+  year: 2023,
 
-// function capitalize(element, index, array) {
-//   array[index] = element[0].toUpperCase() + element.substring(1);
-// }
-
-// function print(element) {
-//   console.log(element);
-// }
-
-// let numbers = [1, 2, 3, 4, 5];
-// let squares = numbers.map(square);
-// let cubes = numbers.map(cube);
-
-// cubes.forEach(print);
-
-// function square(element) {
-//   return Math.pow(element, 2);
-// }
-
-// function cube(element) {
-//   return Math.pow(element, 3);
-// }
-
-// function print(element) {
-//   console.log(element);
-// }
-
-// let ages = [18, 16, 21, 17, 19, 90];
-
-// let adults = ages.filter(checkAge);
-
-// adults.forEach(print);
-
-// function checkAge(element) {
-//   return element >= 18;
-// }
-
-// function print(element) {
-//   console.log(element);
-// }
-
-// let prices = [5, 10, 15, 20, 25, 2];
-
-// let total = prices.reduce(checkOut);
-
-// console.log(`Total is $${total}`);
-
-// function checkOut(total, element) {
-//   return total + element;
-// }
-
-// let grades = [100, 50, 90, 60, 80, 70];
-
-// grades = grades.sort(ascendingSort);
-
-// grades.forEach(print);
-
-// function descendingSort(x, y) {
-//   return y - x;
-// }
-
-// function ascendingSort(x, y) {
-//   return x - y;
-// }
-
-// function print(element) {
-//   console.log(element);
-// }
-
-// sayHello();
-
-// function sayHello() {
-//   console.log("Hello!");
-// }
-
-// let count = 0;
-
-// function increaseCount() {
-//   count++;
-//   document.getElementById("myLabel").innerHTML = count;
-// }
-
-// function decreaseCount() {
-//   count--;
-//   document.getElementById("myLabel").innerHTML = count;
-// }
-
-let count = 0;
-
-document.getElementById("increaseButton").onclick = function () {
-  count++;
-  document.getElementById("myLabel").innerHTML = count;
+  drive: function () {
+    console.log("You drive the car");
+  },
+  brake: function () {
+    console.log("You use brakes");
+  },
 };
-document.getElementById("decreaseButton").onclick = function () {
-  count--;
-  document.getElementById("myLabel").innerHTML = count;
+
+const car2 = {
+  model: "Stinger",
+  color: "grey",
+  year: 2022,
+
+  drive: function () {
+    console.log("You drive the car");
+  },
+  brake: function () {
+    console.log("You use brakes");
+  },
 };
+
+console.log(car2.model);
+console.log(car2.color);
+console.log(car2.year);
